384fd6d8e604dcacdf107b2ac5c34ccc1dddb4c6
miguelogren <miguel.h.ogren@gmail.com>
2021-11-15 14:37:35 +0100

    Demo pull request

---
+++ src/Client/Client.java
@@ -0,0 +1,14 @@
+package Client;
+
+public class Client {
+
+ public Client(){}
+
+ Network network = new Network();
+
+ public static void main(String[] args){
+
+  Client client = new Client();
+
+ }
+}
