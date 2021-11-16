edd19171fe74201ff6212c0e9ba1004063aa2aed
Philip Tonaczew <58476139+Tyareenbajka@users.noreply.github.com>
2021-11-16 12:12:00 +0100

    implemented gameWindow GUI

--- src/Client/GUI/Container.java
+++ src/Client/GUI/Container.java
@@ -7,7 +7,7 @@ public class Container extends JFrame {
 
     String question;
     Window startWindow;
-    Window gameWindow;
+    GameWindow gameWindow;
     myButton startButton = new myButton("Start");
     myButton questionButton;
     myButton[][] answerButtons = new myButton[2][2];
@@ -21,30 +21,14 @@ public class Container extends JFrame {
         setVisible(true);
         setLocationRelativeTo(null);
 
-        add(startWindow = new Window());
-        startWindow.setSize(750,750);
-        startWindow.setBackground(Color.BLACK);
-        startWindow.add(startButton);
-        startButton.addActionListener(e -> System.out.println("hej"));
-        //startButton.addActionListener(new listener());
+        gameWindow = new GameWindow();
 
-        add(gameWindow = new Window());
-        gameWindow.setSize(750, 750);
-        gameWindow.setBackground(Color.CYAN);
-        for (int i = 0; i<2; ++i) {
-            for(int j = 0; i<2; ++j){
-                answerButtons[i][j] = new myButton(answers[i]);
-            }
+        this.add(gameWindow);
 
-        }
+
     }
-    /*
-    //istället för lambda
-    private class listener implements ActionListener{
 
-        @Override
-        public void actionPerformed(ActionEvent e) {
-            System.out.println("hej");
-        }
-    }*/
+    public static void main(String[] args) {
+        new Container();
+    }
 }
--- src/Client/GUI/GameWindow.java
+++ src/Client/GUI/GameWindow.java
@@ -1,8 +1,44 @@
 package Client.GUI;
 
-public class GameWindow {
+import javax.swing.*;
+import java.awt.*;
+
+public class GameWindow extends JPanel {
+
+
 
     public GameWindow(){
 
+        setBackground(Color.yellow);
+        setLayout(new BorderLayout());
+
+        JPanel questionPanel = new JPanel();
+        JLabel question = new JLabel("Whats your name?");
+        questionPanel.add(question);
+        questionPanel.setBackground(Color.white);
+
+        JPanel timerPanel = new JPanel();
+        timerPanel.setPreferredSize(new Dimension(750,250));
+        timerPanel.setBackground(Color.red);
+
+        JPanel buttonsPanel = new JPanel();
+        buttonsPanel.setLayout(new GridLayout(2,2));
+        buttonsPanel.setBackground(Color.GREEN);
+        myButton b1 = new myButton("Button1");
+        myButton b2 = new myButton("Button2");
+        myButton b3 = new myButton("Button3");
+        myButton b4 = new myButton("Button4");
+        buttonsPanel.add(b1);
+        buttonsPanel.add(b2);
+        buttonsPanel.add(b3);
+        buttonsPanel.add(b4);
+
+
+
+        this.add(questionPanel, BorderLayout.NORTH);
+        this.add(timerPanel, BorderLayout.CENTER);
+        this.add(buttonsPanel, BorderLayout.SOUTH);
     }
+
+
 }
