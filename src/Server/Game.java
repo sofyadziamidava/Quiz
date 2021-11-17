5317cd853aa0ecd7dc0581add7d466618e6fd173
rausss <wolf_rosemarie@hotmail.com>
2021-11-17 14:10:05 +0100

    updates game in bransch to developer

--- src/Server/Game.java
+++ src/Server/Game.java
@@ -1,40 +1,94 @@
 package Server;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
-import java.net.Socket;
 
-public class Game extends Thread {
-    Socket socket;
-    Socket socket2;
-    Player player1;
-    Player player2;
+public class Game extends Thread{
 
-    public Game(Socket socket, Socket socket2){
-        this.socket = socket;
-        this.socket2 = socket2;
-        System.out.println("Client connected");
+    int nrOfRounds;
+    int currentRound;
+    Player[] players = new Player[2];
+    int currentPlayer;
+    int[] player1SumPoints = new int[nrOfRounds];
+    int[] player2SumPoints = new int[nrOfRounds];
+
+    public Game(Player player1, Player player2) {
+        this.players[0] = player1;
+        this.players[1] = player2;
+        currentPlayer = 0;
+
     }
 
-    @Override
-    public void run() {
-        try(BufferedReader inputStream = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            PrintWriter outputStream = new PrintWriter(socket.getOutputStream(), true);
-            PrintWriter outputStream2 = new PrintWriter(socket2.getOutputStream(), true))
-        {
-            //outputStream.println("What old man do you never see in the summer?");
-            String paketet = "Paket";
-            outputStream.println(paketet);
-            outputStream2.println(paketet);
-            while(true){
-                String dataFromClient = inputStream.readLine();
-                System.out.println("Data from Client: " + dataFromClient);
-            }
+    public int getNrOfRounds(){
+        return nrOfRounds;
+    }
+    public int getCurrentRound() {
+        return currentRound;
+    }
 
-        }  catch (IOException e) {
+    /*
+    Creating a round and sending it to both clients
+     */
+    public void sendRounds() {
+        Rond rond = new Rond();
+        for (Player player:players) {
+            try {
+                player.send(rond);
+            } catch (IOException e) {
+                e.printStackTrace();
+                System.out.println("could not send round to player");
+            }
+        }
+    }
+
+    public void waitingForAnswers() {
+        int answerFromClient = 0;
+        while(answerFromClient < 2) {
+            String nrOfCorrectAnswers;
+            try {
+                if((nrOfCorrectAnswers = players[0].receive()) != null  ) {
+                    player1SumPoints[currentRound] = Integer.parseInt(nrOfCorrectAnswers);
+                    answerFromClient++;
+                }
+                if((nrOfCorrectAnswers = players[1].receive()) != null  ) {
+                    player2SumPoints[currentRound] = Integer.parseInt(nrOfCorrectAnswers);
+                    answerFromClient ++;
+                }
+            } catch (IOException | ClassNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void sendingOpponentResultToClients() {
+        try {
+            players[0].send(player2SumPoints[currentRound]);
+            players[1].send(player1SumPoints[currentRound]);
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
+
+    public void sendAllOpponentResultsToClient() {
+        try {
+            players[0].send(player2SumPoints);
+            players[1].send(player1SumPoints);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void run() {
+
+        for (currentRound = 0; currentRound < nrOfRounds; currentRound++) {
+            GameProtocol protocol = new GameProtocol(this);
+            protocol.gameProcess();
+        }
+
+        //TODO: how to solve for creating a new game after played???
+
+    }
+
+
+
 }
+
