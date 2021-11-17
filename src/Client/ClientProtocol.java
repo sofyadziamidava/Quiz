c634cfc710c3310471570e869aa15814aed844f4
Sofya Dziamidava <sofya.dziamidava@gmail.com>
2021-11-17 12:25:42 +0100

    started working on ClientProtocol

---
+++ src/Client/ClientProtocol.java
@@ -0,0 +1,10 @@
+package Client;
+
+public class ClientProtocol {
+    private final static int RECIVEROND = 0;
+    private final static int SENDRESULT = 1;
+    private final static int GETOPPONENTSRESULT = 2;
+    private final static int GETFINALRESULT = 3;
+
+
+}
