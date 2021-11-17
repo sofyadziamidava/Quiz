61b4604023e9a640ce6253dc3dcbc440a94284b8
rausss <wolf_rosemarie@hotmail.com>
2021-11-17 12:35:37 +0100

    continued with game execution 2.0

--- src/Server/Game.java
+++ src/Server/Game.java
@@ -4,25 +4,91 @@ import java.io.IOException;
 
 public class Game extends Thread{
 
+    int nrOfRounds;
+    int currentRound;
     Player[] players = new Player[2];
+    int currentPlayer;
+    int[] player1SumPoints = new int[nrOfRounds];
+    int[] player2SumPoints = new int[nrOfRounds];
 
     public Game(Player player1, Player player2) {
         this.players[0] = player1;
         this.players[1] = player2;
+        currentPlayer = 0;
+
+    }
+
+    public int getNrOfRounds(){
+        return nrOfRounds;
+    }
+    public int getCurrentRound() {
+        return currentRound;
+    }
+
+    /*
+    Creating a round and sending it to both clients
+     */
+    public void sendRounds() {
+        Rond rond = new Rond();
+        for (Player player:players) {
+            try {
+                player.send(rond);
+            } catch (IOException e) {
+                e.printStackTrace();
+                System.out.println("could not send round to player");
+            }
+        }
+    }
+
+    public void waitingForAnswers() {
+        int answerFromClient = 0;
+        while(answerFromClient < 2) {
+            String nrOfCorrectAnswers;
+            try {
+                if((nrOfCorrectAnswers = players[0].receive()) != null  ) {
+                    player1SumPoints[currentRound] = Integer.parseInt(nrOfCorrectAnswers);
+                    answerFromClient++;
+                }
+                if((nrOfCorrectAnswers = players[1].receive()) != null  ) {
+                    player2SumPoints[currentRound] = Integer.parseInt(nrOfCorrectAnswers);
+                    answerFromClient ++;
+                }
+            } catch (IOException | ClassNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void sendingOpponentResultToClients() {
+        try {
+            players[0].send(player2SumPoints[currentRound]);
+            players[1].send(player1SumPoints[currentRound]);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void sendAllOpponentResultsToClient() {
+        try {
+            players[0].send(player2SumPoints);
+            players[1].send(player1SumPoints);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public void run() {
 
-        //TODO: STATES:
-        // * spelet startar -> server skickar första ronden
-        // * resultat kommer in -> skickar till båda klienter
-        // * sista resultat kommer in -> skickar alla resultat till båda klienter och frågar om nytt spel
+        for (currentRound = 0; currentRound < nrOfRounds; currentRound++) {
+            GameProtocol protocol = new GameProtocol(this);
+            protocol.gameProcess();
+        }
 
-        GameProtocol gameProtocol = new GameProtocol(players);
-        gameProtocol.gameProcess();
+        //TODO: how to solve for creating a new game after played???
+
     }
 
 
 
-
 }
+
--- src/Server/GameProtocol.java
+++ src/Server/GameProtocol.java
@@ -7,76 +7,33 @@ public class GameProtocol {
     public static final int WAITINGFORRESULTS = 1;
     public static final int SENDINGRESULTS = 2;
     private static final int ENDRESULT = 3;
+    public int state = NEWROND;
 
-    private static final int NUMBEROFRONDS = 1;
+    Game game;
 
-    private int state = NEWROND;
-    private int currentRond = 0;
-
-    private Rond rond1 = new Rond();
-    private Rond rond2 = new Rond();
-    private Rond[] ronds = new Rond[]{rond1, rond2};
-
-    private Player players[];
-
-    public GameProtocol(Player[] players) {
-        this.players = players;
+    public GameProtocol(Game game) {
+        this.game = game;
     }
-
-    private Object[] player1Results = new Object[2];
-    private Object[] player2Results = new Object[2];
-
 
     public void gameProcess()  {
-        ResultThread rtPlayer1 = new ResultThread(players[0]);
-        ResultThread rtPlayer2 = new ResultThread(players[1]);
+
         if (state == NEWROND) {
-            try {
-                for (Player player : players) {
-                    player.send(ronds[currentRond]);
-                }
-            } catch (IOException e){
-                System.out.println("error: " + e.getMessage());
-            }
-            currentRond++;
+            game.sendRounds();
             state = WAITINGFORRESULTS;
         } else if (state == WAITINGFORRESULTS){
-            rtPlayer1.run();
-            rtPlayer2.run();
-            if(rtPlayer1.getResult() != null && rtPlayer2.getResult() != null) {
-                state = SENDINGRESULTS;
+            game.waitingForAnswers();
+            state = SENDINGRESULTS;
             }
-        } else if (state == SENDINGRESULTS) {
-            try{
-            for (Player player : players) {
-                player.send(rtPlayer1.getResult());
-                player.send(rtPlayer2.getResult());
-            }} catch (IOException e){
-                System.out.println("error: " + e.getMessage());
-            }
-            for (int i = 0; i < players.length; i++) {
-                player1Results[i] = rtPlayer1.getResult();
-                player2Results[i] = rtPlayer2.getResult();
-            }
-            rtPlayer1.setResult(null);
-            rtPlayer2.setResult(null);
-            if(currentRond < NUMBEROFRONDS){
-                state =NEWROND;
-            } else{
-                state = ENDRESULT;
-            }
+         else if (state == SENDINGRESULTS) {
+             game.sendingOpponentResultToClients();
+             if (game.getCurrentRound() == game.getNrOfRounds()-1) {
+                 state = ENDRESULT;
+             }
         } else if(state == ENDRESULT){
-            for (Player player: players) {
-                for (int i = 0; i < players.length; i++) {
-                    try {
-                        player.send(player1Results[i]);
-                    } catch (IOException e){
-                        System.out.println("error: " + e.getMessage());
-                    }
-                }
-            }
+            game.sendAllOpponentResultsToClient();
         }
     }
+
 
 
 
