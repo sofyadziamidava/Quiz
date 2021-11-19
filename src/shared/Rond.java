24d3f36bd7d5dcecc4d947b446c615f23e70816d
rausss <wolf_rosemarie@hotmail.com>
2021-11-19 11:07:59 +0100

    properties file added

--- src/shared/Rond.java
+++ src/shared/Rond.java
@@ -4,19 +4,15 @@ import java.io.Serializable;
 import java.util.ArrayList;
 
 public class Rond implements Serializable {
-    Question question1;
-    Question question2;
 
-    public Rond(ArrayList<Question> q) {
-        this.question1 = q.get(0);
-        this.question2 = q.get(1);
+    ArrayList<Question> questionList;
+
+    public Rond(ArrayList<Question> questions) {
+
+        this.questionList = questions;
     }
 
-    public Question getQuestion1() {
-        return question1;
-    }
-
-    public Question getQuestion2() {
-        return question2;
+    public ArrayList<Question> getQuestionList() {
+        return questionList;
     }
 }
