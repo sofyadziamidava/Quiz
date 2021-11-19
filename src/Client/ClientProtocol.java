7a36d52d0f978e548cc438b30e9dbf7bd8a951fd
Philip Tonaczew <58476139+Tyareenbajka@users.noreply.github.com>
2021-11-19 09:33:04 +0100

    changed name on the new methods in ClientProtocol

--- src/Client/ClientProtocol.java
+++ src/Client/ClientProtocol.java
@@ -5,7 +5,6 @@ import Client.GUI.Window;
 import shared.Question;
 import shared.Rond;
 
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
@@ -27,18 +26,39 @@ public class ClientProtocol {
         Rond newRond = (Rond)o;
         List<Question> questionList = newRond.getQuestionList();
 
-        for(Question q : questionList){
-            playRound(q);
+        for(Question currentQuestion : questionList){
+            playRound(currentQuestion);
         }
 
         //send points to server
     }
 
-    private void playRound(Question q) {
-        unpackQuestion(q);
-        Collections.shuffle(answers);
-        createGameWindow(question, answers, correctAnswer);
+    private void playRound(Question currentQuestion) {
+        unpackCurrentQuestion(currentQuestion);
+        createGameWindowFromCurrentQuestion(question, answers, correctAnswer);
+        playCurrentQuestion();
+    }
 
+    private void unpackCurrentQuestion(Question q) {
+        this.question = q.getQuestion();
+        this.answers = q.getAnswers();
+        this.correctAnswer = answers.get(0);
+        Collections.shuffle(answers);
+    }
+
+    private void createGameWindowFromCurrentQuestion(String question, List<String> alternatives, String correctAnswer) {
+        window.dispose();
+        this.gameWindow = new Window();
+        this.gamePanel = gameWindow.getGameWindow();
+        this.gameWindow.add(gamePanel);
+        this.gameWindow.setVisible(true);
+
+        this.gamePanel.displayQuestion(question);
+        this.gamePanel.displayButtons(alternatives);
+        this.gamePanel.setCorrectAnswer(correctAnswer);
+    }
+
+    private void playCurrentQuestion() {
         boolean checkTimer = gamePanel.getTimerLabel().getText().equals("0");
         while(!checkTimer){
             ClientNetwork.sleep();
@@ -48,40 +68,7 @@ public class ClientProtocol {
             }
             checkTimer = gamePanel.getTimerLabel().getText().equals("0");
         }
-
         System.out.println("out of while loop");
         this.gameWindow.dispose();
     }
-
-    private void unpackQuestion(Question q) {
-        this.question = q.getQuestion();
-        this.answers = q.getAnswers();
-        this.correctAnswer = answers.get(0);
-    }
-
-    private void createGameWindow(String question, List<String> alternatives, String correctAnswer) {
-        window.dispose();
-        this.gameWindow = new Window();
-        this.gamePanel = gameWindow.getGameWindow();
-        gameWindow.add(gamePanel);
-        gameWindow.setVisible(true);
-
-        this.gamePanel.displayQuestion(question);
-        this.gamePanel.displayButtons(alternatives);
-        this.gamePanel.setCorrectAnswer(correctAnswer);
-    }
-
-
-
-
-    public void sendPointsToServer(int points){
-
-    }
-
-    public void reciveRondsResult(){
-
-    }
-
-
-
 }
