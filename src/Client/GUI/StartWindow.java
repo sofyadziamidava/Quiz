0b862114ff426cb69b724bf127cdef759cd42dbc
Philip Tonaczew <58476139+Tyareenbajka@users.noreply.github.com>
2021-11-24 14:23:05 +0100

    StartWindow gui

--- src/Client/GUI/StartWindow.java
+++ src/Client/GUI/StartWindow.java
@@ -7,34 +7,66 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import static javax.swing.BoxLayout.Y_AXIS;
+
 public class StartWindow extends JPanel{
 
     String savedName;
     JTextArea name;
-    JPanel nameTextPanel;
+    JPanel loginPanel;
     JLabel nameLabel;
     myButton startButton;
     boolean ready;
 
     public StartWindow() {
-        ready = false;
-        StartAction buttonAction = new StartAction();
-        setBackground(Color.blue);
 
+        // title panel
+        JPanel titlePanel = new JPanel();
+        titlePanel.setLayout(new GridLayout());
+        JLabel title = new JLabel("QuizCamp");
+        title.setFont(customFont(40));
+        titlePanel.setBackground(Color.GREEN);
+        title.setHorizontalAlignment(JLabel.CENTER);
 
-        nameTextPanel = new JPanel();
-        nameLabel = new JLabel("Enter name");
-        startButton = new myButton("Start");
+        titlePanel.add(title);
+
+        // login panel
+        loginPanel = new JPanel();
+        loginPanel.setLayout(new FlowLayout());
+        loginPanel.setBackground(Color.yellow);
+        nameLabel = new JLabel("Enter name:");
+        nameLabel.setFont(customFont(20));
+        nameLabel.setHorizontalAlignment(JLabel.CENTER);
         name = new JTextArea();
+        name.setFont(customFont(20));
         name.setPreferredSize(new Dimension(100, 30));
 
-        add(nameTextPanel);
-        nameTextPanel.add(nameLabel);
-        add(name);
-        add(startButton);
+        loginPanel.add(nameLabel);
+        loginPanel.add(name);
+
+
+        //botton panel
+        JPanel botton = new JPanel();
+        botton.setBackground(Color.black);
+        startButton = new myButton("Start");
+        botton.add(startButton);
+
+
+        ready = false;
+        StartAction buttonAction = new StartAction();
+
+
+
+
+
 
         startButton.addActionListener(buttonAction);
 
+        this.setBackground(new Color(205,230,255));
+        this.setLayout(new GridLayout(3,1));
+        this.add(titlePanel);
+        this.add(loginPanel);
+        this.add(botton);
     }
 
     public String getSavedName() {
@@ -56,5 +88,16 @@ public class StartWindow extends JPanel{
 
     public boolean getReady() {
         return ready;
+    }
+
+    private Font customFont(int size){
+        return new Font("Arial", Font.BOLD, size);
+    }
+
+    public static void main(String[] args) {
+        Window window = new Window();
+        StartWindow startWindow = new StartWindow();
+        window.add(startWindow);
+        window.setVisible(true);
     }
 }
